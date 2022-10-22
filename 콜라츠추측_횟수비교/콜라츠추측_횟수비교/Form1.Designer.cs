879d4da287004c277e8fb25e4ecef32a0fb1668f
Thehighestpride <90371479+Thehighestpride@users.noreply.github.com>
2022-10-22 20:16:26 +0900

    Add files via upload

---
+++ 콜라츠추측_횟수비교/콜라츠추측_횟수비교/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace 콜라츠추측_횟수비교
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.NaturalNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CalcTimes = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BiggestNum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnInitialize = new System.Windows.Forms.Button();
+            this.btnRank = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 64.45959F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35.54041F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1027, 689);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NaturalNum,
+            this.CalcTimes,
+            this.BiggestNum});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(656, 683);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // NaturalNum
+            // 
+            this.NaturalNum.HeaderText = "X값";
+            this.NaturalNum.Name = "NaturalNum";
+            // 
+            // CalcTimes
+            // 
+            this.CalcTimes.HeaderText = "계산횟수";
+            this.CalcTimes.Name = "CalcTimes";
+            // 
+            // BiggestNum
+            // 
+            this.BiggestNum.HeaderText = "최대값";
+            this.BiggestNum.Name = "BiggestNum";
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.listBox1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(665, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 74.37775F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25.62226F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(359, 683);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.Font = new System.Drawing.Font("궁서", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(3, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(353, 501);
+            this.listBox1.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.btnClose);
+            this.panel1.Controls.Add(this.btnInitialize);
+            this.panel1.Controls.Add(this.btnRank);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 510);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(353, 170);
+            this.panel1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("궁서", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2.Location = new System.Drawing.Point(14, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 74);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "계산";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSave.Font = new System.Drawing.Font("궁서", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnSave.Location = new System.Drawing.Point(236, 8);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(105, 74);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "저장";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Font = new System.Drawing.Font("궁서", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnClose.Location = new System.Drawing.Point(232, 88);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(109, 74);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "종료";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnInitialize
+            // 
+            this.btnInitialize.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInitialize.Font = new System.Drawing.Font("궁서", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnInitialize.Location = new System.Drawing.Point(14, 88);
+            this.btnInitialize.Name = "btnInitialize";
+            this.btnInitialize.Size = new System.Drawing.Size(212, 74);
+            this.btnInitialize.TabIndex = 2;
+            this.btnInitialize.Text = "초기화";
+            this.btnInitialize.UseVisualStyleBackColor = true;
+            this.btnInitialize.Click += new System.EventHandler(this.btnInitialize_Click);
+            // 
+            // btnRank
+            // 
+            this.btnRank.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRank.Font = new System.Drawing.Font("궁서", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnRank.Location = new System.Drawing.Point(125, 10);
+            this.btnRank.Name = "btnRank";
+            this.btnRank.Size = new System.Drawing.Size(105, 74);
+            this.btnRank.TabIndex = 0;
+            this.btnRank.Text = "정리";
+            this.btnRank.UseVisualStyleBackColor = true;
+            this.btnRank.Click += new System.EventHandler(this.btnRank_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 689);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "<Collatz Guessing Number Comparison> Ver 1.0.3";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NaturalNum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CalcTimes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BiggestNum;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnRank;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnInitialize;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ 콜라츠추측_횟수비교/콜라츠추측_횟수비교/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace 콜라츠추측_횟수비교
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        long CalcSequence = 1, Sequence = 1, Biggest = default;
+        int times = 0;
+        
+        bool flag = true;
+
+        string fileName = "noname.txt";
+
+        List<long> rankNums = new List<long>();
+
+        Func<long, long> Distinguish = x => 
+        {
+            if (x % 2 == 0)
+                return x / 2;
+            else  if (x == 1)
+                return 1;
+            else
+                return x * 3 + 1;
+        };
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnRank_Click(object sender, EventArgs e)
+        {
+            if(timer1.Enabled == true)
+            {
+                MessageBox.Show("계산하는중에는 정리가 불가능합니다.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            RankNums();
+        }
+
+        private void RankNums()
+        {
+            if (listBox1.Items.Count == 0)
+                return;
+
+            try
+            {
+                for (int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    rankNums.Add(Convert.ToInt64(listBox1.Items[i]));
+                }
+
+                rankNums.Sort();
+
+                listBox1.Items.Clear();
+
+                long biggest = rankNums[rankNums.Count - 1]; // 리스트박스에서 가장 큰 숫자
+                int count = 0;
+
+                for (int i = 1; i <= biggest; i++) // 리스트박스에서 가장 큰 숫자만큼 반복
+                {
+                    for (int j = 0; j < rankNums.Count; j++) // 만약 listBox1.Item[j]가 i랑 같다면 나온횟수 +1
+                    {
+                        if (rankNums[j] == i)
+                            count++;
+                    }
+
+                    if(count != 0)
+                        listBox1.Items.Add(String.Format("<{0}>: {1}번", i, count));
+                    count = 0;
+                }
+
+                MessageBox.Show("정리완료!", "알림", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch(FormatException fex)
+            {
+                MessageBox.Show(fex.Message);
+            }
+        }
+
+        private void btnInitialize_Click(object sender, EventArgs e)
+        {
+            CalcSequence = 1; Sequence = 1; Biggest = default;
+
+            rankNums.Clear();
+            listBox1.Items.Clear();
+            dataGridView1.Rows.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (flag == true)
+            {
+                timer1.Enabled = true;
+
+                listBox1.Items.Clear();
+
+                for (int i = 0; i < rankNums.Count; i++)
+                    listBox1.Items.Add(rankNums[i]);
+
+                flag = false;
+            }
+            else
+            {
+                timer1.Enabled = false;
+                flag = true;
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+            {
+                MessageBox.Show("계산하는중에는 저장이 불가능합니다.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            else if (listBox1.Items.Count == 0) { return; }
+
+            btnRank_Click(btnRank, null);
+
+            string saveRanks = "";
+
+            for(int i = 0; i < listBox1.Items.Count; i++)
+            {
+                saveRanks += listBox1.Items[i].ToString() + "\n";
+            }
+
+            if (fileName == "noname.txt")
+            {
+                saveFileDialog1.ShowDialog();
+                fileName = saveFileDialog1.FileName;
+            }
+            StreamWriter sw = File.CreateText(fileName);
+            sw.WriteLine(saveRanks);
+            sw.Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) // 계산
+        {
+            CalcSequence = Distinguish(CalcSequence);
+            listBox1.Items.Add(CalcSequence);
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+
+            rankNums.Add(Convert.ToInt32(CalcSequence));
+
+            if (CalcSequence > Biggest)
+            Biggest = CalcSequence;
+                
+            times++;
+
+            if(CalcSequence == 1)
+            {
+                dataGridView1.Rows.Add(Sequence, times, Biggest);
+                times = 0;
+                Sequence++;
+                CalcSequence = Biggest = Sequence;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 7;
+        }
+    }
+}
